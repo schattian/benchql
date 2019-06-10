aac1f088c2cd26a4ec944c19479eeed32c9c021e
Sebastian Chamena <sebastian.chamena@talkiu.com>
2019-06-10 20:10:54 -0300

    Adds thread management through config

--- benchql.py
+++ benchql.py
@@ -30,12 +30,12 @@ def parse_duration(time_str):
     return timedelta(**time_params).total_seconds()
 
 
-async def run_wrk(endpoint, query, concurrency=20, duration=10):
+async def run_wrk(endpoint, query, concurrency=20, duration=10,threads=1):
     # wrk = await asyncio.create_subprocess_exec(
     #     'wrk2', '-R', str(500), '-t', '1', '-c', str(concurrency), '-d', str(int(duration)), '-s', 'misc/graphql.lua',
     #     endpoint, query, stdout=PIPE, stderr=PIPE)
     wrk = await asyncio.create_subprocess_exec(
-        'wrk', '-t', '1', '-c', str(concurrency), '-d', str(int(duration)), '-s', 'misc/graphql.lua',
+        'wrk', '-t', str(int(threads)), '-c', str(concurrency), '-d', str(int(duration)), '-s', 'misc/graphql.lua',
         endpoint, query, stdout=PIPE, stderr=PIPE)
 
     output, json_data = await wrk.communicate()
@@ -58,41 +58,19 @@ async def run_wrk(endpoint, query, concurrency=20, duration=10):
     return data, response
 
 
-
 async def start_server(command, cwd, wait_for=None):
     server = await asyncio.create_subprocess_exec(*command.split(), cwd=cwd)
-
-    # lines = []
-    # async for line in server.stdout:
-    #     print(line.decode("utf-8"),)
-
-    # while True:
-    #     line = await server.stdout.readline()
-    #     print("READ",  line)
-    #     if line:
-    #         line = line.decode('utf-8')
-    #         lines.append(line)
-    #     else:
-    #         break
-
-    # while 1:
-    #     line = await server.stderr.readline()
-    #     print(line)
-    #     if not line:
-    #         break
     return server
-    # asyncio.wait_for
-    # retcode = await server.wait()
 
 
-async def get_query_result(query_name, server_name, url, query_filename, warmup_duration=0, warmup_concurrency=1):
+async def get_query_result(query_name, server_name, url, query_filename, warmup_duration=0, warmup_concurrency=1, threads=1):
     print("- Running query {}".format(query_name, server_name))
     if warmup_duration:
         print("  > Warming up the server")
-        await run_wrk(url, query_filename, concurrency=warmup_concurrency, duration=warmup_duration)
+        await run_wrk(url, query_filename, concurrency=warmup_concurrency, duration=warmup_duration, threads=threads)
         await asyncio.sleep(2)
     print("  > Benchmarking")
-    result, response = await run_wrk(url, query_filename)
+    result, response = await run_wrk(url, query_filename, threads)
     return ({
         "query_name": query_name,
         "server_name": server_name,
@@ -113,7 +91,7 @@ async def force_termination(pid):
     await kill.communicate()
 
 
-async def bench_server(name, command, cwd, endpoint, queries, warmup_duration=0, command_ready_seconds=2, warmup_concurrency=1):
+async def bench_server(name, command, cwd, endpoint, queries, warmup_duration=0, command_ready_seconds=2, warmup_concurrency=1, threads=1):
     print("Starting server: {}".format(name))
     if not await endpoint_is_dead(endpoint):
         print("- Can't start the server as there is a process already running on {}".format(endpoint))
@@ -125,7 +103,7 @@ async def bench_server(name, command, cwd, endpoint, queries, warmup_duration=0,
         for query in queries:
             result, response = None, None
             try:
-                result, response = await get_query_result(query.get('name'), name, endpoint, query.get('filename'), warmup_duration, warmup_concurrency)
+                result, response = await get_query_result(query.get('name'), name, endpoint, query.get('filename'), warmup_duration, warmup_concurrency, threads=threads)
                 results.append(
                     result
                 )
@@ -148,8 +126,9 @@ async def bench_server(name, command, cwd, endpoint, queries, warmup_duration=0,
         if server.returncode is None:
             server.terminate()
         await server.wait()
-        await asyncio.sleep(1)
-        # await force_termination(server.pid)
+        await asyncio.sleep(2)
+        if server.returncode is None:
+            await force_termination(server.pid)
         await asyncio.sleep(1)
         print("Terminated")
 
@@ -174,7 +153,7 @@ async def _main(config, output):
     with open(config, 'r') as content_file:
         content = content_file.read()
         config = yaml.load(content, Loader=yaml.FullLoader)
-
+        warmup = config.get('warmup') or {}
         all_queries = config.get('queries')
         all_servers = config.get('servers')
         for query in all_queries:
@@ -184,17 +163,20 @@ async def _main(config, output):
                     content = content_file.read()
                     query['expectedResult'] = json.loads(content)
 
+    duration = parse_duration(warmup.get('duration') or 0)
+    concurrency = warmup.get('concurrency') or 1
+    threads = warmup.get('threads') or 1
+
     results = []
     for server in all_servers:
+        if 'enabled' in server.keys() and not bool(server['enabled']):
+            continue
         server_name = server.get('name')
         server_run = server.get('run')
         server_command = server_run.get('command')
         server_cwd = server_run.get('cwd') or "./"
         server_wait = parse_duration(server_run.get('startupTime', 2))
         server_endpoint = server.get('endpoint')
-        server_warmup = server.get('warmup') or {}
-        server_warmup_duration = parse_duration(server_warmup.get('duration') or 0)
-        server_warmup_concurrency = server_warmup.get('concurrency') or 1
 
         results += await bench_server(
             name=server_name,
@@ -202,10 +184,11 @@ async def _main(config, output):
             cwd=os.path.join(dir_path, server_cwd),
             endpoint=server_endpoint,
             queries=all_queries,
-            warmup_duration=server_warmup_duration,
-            warmup_concurrency=server_warmup_concurrency,
+            warmup_duration=duration,
+            warmup_concurrency=concurrency,
             command_ready_seconds=server_wait,
-        )
+            threads=threads
+            )
 
     if output:
         with open(output, 'w') as output_file:
